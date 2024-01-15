2f7b45e0d450249f3d5197351dd7b178cc31130c
alpacaive <w6352800@naver.com>
2024-01-15 22:29:12 +0900

    서비스 계층과 트랜잭션 서비스 계층 만들기 게시글 조회 요청 개선(모든 게시글 조회 요청 개선, 단일 게시글 조회 요청 개선) 게시글 생성 요청 개선 게시글 수정 요청 개선 게시글 삭제 요청 개선 트랜잭션 테스트 메서드 만들기 - 강제 예외 발생 시키는 메서드 실행시켜 트랜잭션 설정 후 롤백되는 것 확인

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,6 +3,7 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,54 +16,49 @@ import java.util.List;
 @RestController
 public class ArticleApiController {
     @Autowired
-    private ArticleRepository articleRepository;
+    private ArticleService articleService; // 서비스 객체 주입
     // 1. GET
     @GetMapping("/api/articles")
     public List<Article> index() {
-        return articleRepository.findAll();
+        return articleService.index();
     }
     @GetMapping("/api/articles/{id}")
     public Article show(@PathVariable Long id) {
-        return articleRepository.findById(id).orElse(null);
+        return articleService.show(id);
     }
     // 2. POST
     @PostMapping("/api/articles")
-    public Article create(@RequestBody ArticleForm dto) {
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto) { // 반환형 수정
+        // POST 요청에 대한 응답으로 ResponseEntity에 응답 실어 보냄
         // @RequestBody : 요청 시 본문에 실어 보내는 데이터를 create() 메서드의 매개변수로 받아올 수 있음
-        Article article = dto.toEntity();
-        return articleRepository.save(article);
+        Article created = articleService.create(dto);
+        return (created != null) ? // 생성하면 정상, 실패하면 오류 응답
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
     // 3. PATCH
     @PatchMapping("/api/articles/{id}")
     public ResponseEntity<Article> update(@PathVariable Long id,
-                          @RequestBody ArticleForm dto) {
-        // 1. DTO -> 엔티티 변환하기
-        Article article = dto.toEntity();
-        log.info("id: {}, article: {}", id, article.toString());
-        // 2. 타깃 조회하기
-        Article target = articleRepository.findById(id).orElse(null);
-        // 3. 잘못된 요청 처리하기
-        if (target == null || id != article.getId()) {
-            // 400, 잘못된 요청 응답
-            log.info("잘못된 요청! id: {}, article: {}", id, article.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 4. 업데이트 및 정상 응답(200)하기
-        target.patch(article);
-        Article updated = articleRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
+                                          @RequestBody ArticleForm dto) {
+        Article updated = articleService.update(id, dto); // 서비스를 통해 게시글 수정
+        return (updated != null) ? // 수정되면 정상, 안 되면 오류 응답
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
     // 4. DELETE
     @DeleteMapping("api/articles/{id}")
     public ResponseEntity<Article> delete(@PathVariable Long id) {
-        // 1. 대상 찾기
-        Article target = articleRepository.findById(id).orElse(null);
-        // 2. 잘못된 요청 처리하기
-        if (target == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 3. 대상 삭제하기
-        articleRepository.delete(target);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        Article deleted = articleService.delete(id); // 서비스를 통해 게시글 삭제
+        return (deleted != null) ? // 삭제 결과에 따라 응답 처리
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build() :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+    @PostMapping("/api/transaction-test") // 여러 게시글 생성 요청 접수
+    public ResponseEntity<List<Article>> transactionTest
+            (@RequestBody List<ArticleForm> dtos) { // transactionTest() 메서드 정의
+        List<Article> createdList = articleService.createArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
